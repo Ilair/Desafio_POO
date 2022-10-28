bd809da6685d098f72de6650f354861442678ee0
Ilair dos Santos <ilair30@gmail.com>
2022-10-27 22:52:03 -0300

    remove comments

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,6 +1,5 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
         public override void InstalarAplicativo(string nomeApp)
