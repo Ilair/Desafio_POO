85a6c45cea291b4911634423846bc424b54de77d
Ilair dos Santos <ilair30@gmail.com>
2022-10-27 22:49:12 -0300

    final committed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "55997548541", modelo:"Nokia C20", imei:"111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Discord");
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+
+Smartphone iphone = new Iphone(numero: "5599658745", modelo:"Iphone 13 Pró max", imei:"234567891", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
